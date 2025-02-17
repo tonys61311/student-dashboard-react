3f21deb36c4e381d2f07ba6d055e7c533512f5ec
Hsing-Chen <tonys61311@yahoo.com.tw>
2025-02-17 01:06:31 -0500

    add homepage setting

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
+  base: "/student-dashboard-react/",
   plugins: [react(), tsconfigPaths(), svgr()],
 });
