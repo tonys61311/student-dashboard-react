fc219d673a0a6447cd2a728d952feb2bc46f140e
Hsing-Chen <tonys61311@yahoo.com.tw>
2025-02-17 01:21:10 -0500

    modify background image URL

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,7 +12,7 @@ import { RequestStatus } from "@/types/RequestStatus";
 const AppContainer = styled.div`
   width: 100vw;
   height: 100vh;
-  background-image: url("/images/background.svg");
+  background-image: url("/student-dashboard-react/images/background.svg");
   background-size: cover;
   background-position: center;
   display: flex;
